e23d20854d4e6601e214673ee64da42687c1c4ec
alexkinsey <alexanderkinsey@icloud.com>
2021-03-10 18:25:43 +0000

    added 404 page and edited CSS

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -15,31 +15,15 @@ def index():
 
 @app.route('/orders/<index>', methods=['GET'])
 def show_order(index):
-    order = orders_list[int(index) - 1]
-    return render_template(
+    try:
+        order = orders_list[int(index) - 1]
+        return render_template(
         'order.html',
         order = order
-    )
+        )
+    except:
+        return page_not_found(404)
 
-# @app.route('/api/v1/resources/books', methods=['GET'])
-# def api_id():
-#     # Check if an ID was provided as part of the URL.
-#     # If ID is provided, assign it to a variable.
-#     # If no ID is provided, display an error in the browser.
-#     if 'id' in request.args:
-#         id = int(request.args['id'])
-#     else:
-#         return "Error: No id field provided. Please specify an id."
-
-#     # Create an empty list for our results
-#     results = []
-
-#     # Loop through the data and match results that fit the requested ID.
-#     # IDs are unique, but other fields might return many results
-#     for book in books:
-#         if book['id'] == id:
-#             results.append(book)
-
-#     # Use the jsonify function from Flask to convert our list of
-#     # Python dictionaries to the JSON format.
-#     return results
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
--- app/models/orders_list.py
+++ app/models/orders_list.py
@@ -1,7 +1,7 @@
 from app.models.order import Order
 
 order1 = Order("James", "21/02/21", 3)
-order2 = Order("Cara", "10/02/21", 1)
+order2 = Order("Lucy", "10/02/21", 1)
 order3 = Order("Jade", "01/03/21", 5)
 
 orders_list = [order1, order2, order3]
