6b8a031f819f756b8bfe6c34c0955f0a3cb0e8b2
alexkinsey <alexanderkinsey@icloud.com>
2021-03-10 15:48:52 +0000

    implemented orders in HTML

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -2,9 +2,12 @@ from flask import render_template
 
 from app import app
 
+from app.models.orders_list import orders_list
+
 @app.route('/orders')
 def index():
     return render_template(
         'index.html', 
         title='Orders', 
+        orders = orders_list
     )
